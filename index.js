05e2f40a5aca5861d48c4f63c4870cb84404e4ff
Your Name <you@example.com>
2020-10-05 16:46:27 +0900

    routing step 1

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var express = require('express')
 var mongoose = require('mongoose')
 require('dotenv').config()
-
+var apiRouter = require('./routers/routes')
+var path  = require('path')
+var bodyParser = require('body-parser')
 var app = express()
 
+app.set('views', path.resolve(__dirname +'/views'))
+// console.log(path.resolve(__dirname +'/views'))
+app.set('view engine', 'ejs')
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+app.use('/api', apiRouter)
 
 var mongo_url = process.env.MONGO_URL
 mongoose.connect(mongo_url , { useNewUrlParser: true , useUnifiedTopology: true })
-
-
-app.get('/', function(request , response){
-    // console.log(request)
-    response.send("Hello World!!!!!!!!")
-})
 
 
 var port =process.env.PORT || 8080
